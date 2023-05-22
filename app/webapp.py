993d282c2158a8303f1f84b1f53e74f2fcefc954
Maylla Krislainy <mayllak@hotmail.com>
2023-05-22 18:25:12 -0300

    FEAT: creating the database (migrations)

--- app/webapp.py
+++ app/webapp.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,7 +11,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 # login_manager.login_view = "auth.login"
 login_manager.login_message_category = "info"
-
+migrate =  Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -18,8 +19,9 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'data.sqlite')}"
 
     # init extensions
-    login_manager.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from .controllers import blueprints
