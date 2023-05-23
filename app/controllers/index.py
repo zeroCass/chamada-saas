0cfcd3c5e2dbdfbfa488cdf0befe879bd1343775
Mateus Valerio <mateusvalerio1@gmail.com>
2023-05-23 18:16:02 -0300

    feat: session user-type
    now the app lets only the "professor" to see the add button in Turmas page. THis was implemented using the session present in flask to storage the user_type

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -7,5 +7,5 @@ bp = Blueprint("index", __name__)
 @bp.route("/", methods=["GET"])
 @login_required
 def index():
-    # print(f"turmas do usuario: {current_user.turmas}")
+    print(f"turmas do usuario: {current_user.turmas}")
     return render_template("index.jinja2", user=current_user)
