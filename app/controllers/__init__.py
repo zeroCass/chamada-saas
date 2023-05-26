741ee8edd286f9255fa70606895852ad76cc1f35
Maylla Krislainy <mayllak@hotmail.com>
2023-05-26 17:23:25 -0300

    feat: create aula and fix details

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,4 +1,5 @@
 def blueprints():
     from .turmas_controller import bp as turmas_bp
     from .qrcode_controller import bp as qrcode_bp
-    return [turmas_bp, qrcode_bp]
+    from .aulas_controller import bp as aulas_bp
+    return [turmas_bp, qrcode_bp, aulas_bp]
