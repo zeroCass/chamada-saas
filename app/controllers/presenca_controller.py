8b17977c6d6a4dd3348dfb8ffb7319c3592d98bf
Maylla Krislainy <mayllak@hotmail.com>
2023-05-29 19:36:59 -0300

    feat: "aluno" can sign "presenca"

--- app/controllers/presenca_controller.py
+++ app/controllers/presenca_controller.py
@@ -1,7 +1,8 @@
 from ..webapp import db
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_required
-from ..models import Aula
+from flask_login import login_required, current_user
+from datetime import datetime
+from ..models import Aula, Presenca
 
 bp = Blueprint("presenca", __name__)
 
@@ -11,7 +12,23 @@ def register_blueprint(parent_blueprint: Blueprint):
         bp, url_prefix=f"/<int:aula_id>/presenca")
 
 
-@bp.route("/", methods=["POST", "GET"])
+@bp.route("/", methods=["POST"])
 @login_required
 def registrar_presenca(turma_id, aula_id):
-    return f"<h1>Aula ID: {aula_id} </h1>"
+        data_atual = datetime.now()
+        aluno_id = current_user.id
+
+        # Verifica se o aluno já possui uma presença registrada para essa aula
+        presenca_existente = Presenca.query.filter_by(aula_id=aula_id, aluno_id=aluno_id).first()
+        if presenca_existente:
+            flash("Presença já foi registrada", category="warning")
+            return redirect(url_for("turmas.show", turma_id=turma_id))
+        
+        presenca = Presenca(aula_id=aula_id, data=data_atual, aluno_id=aluno_id)
+
+        db.session.add(presenca)
+        db.session.commit()
+
+        flash("Chamada Assinada", category="sucess")
+        return redirect(url_for("turmas.show", turma_id=turma_id))
+        
